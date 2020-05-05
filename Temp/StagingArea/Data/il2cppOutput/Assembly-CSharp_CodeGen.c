0dbe4f3ef8bd80029bfbbca86da444e6a10a5784
Gloria Fan Duan <gduan@saic.edu>
2020-05-05 16:30:12 -0500

    1st commit
    testing assets and video

---
+++ Temp/StagingArea/Data/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,111 @@
+﻿#include "il2cpp-config.h"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+IL2CPP_EXTERN_C_BEGIN
+IL2CPP_EXTERN_C_END
+
+
+
+
+// 0x00000001 System.Void DesertControllerInteraction::Awake()
+extern void DesertControllerInteraction_Awake_mCA7E938800DD01D086F056F146F9FC42A5C8FB85 ();
+// 0x00000002 System.Void DesertControllerInteraction::Update()
+extern void DesertControllerInteraction_Update_mB19C0D597A6804D6FCC7C34DCA2BC6706EF6F4E3 ();
+// 0x00000003 System.Void DesertControllerInteraction::FixedUpdate()
+extern void DesertControllerInteraction_FixedUpdate_mB046CB6568838D4FEF9F97EC6CD2AFCE1C5C580F ();
+// 0x00000004 System.Void DesertControllerInteraction::OnTriggerEnter(UnityEngine.Collider)
+extern void DesertControllerInteraction_OnTriggerEnter_m081E43E2B2ECE2A3CF11CCC0E970E3C3B3831715 ();
+// 0x00000005 System.Void DesertControllerInteraction::OnTriggerExit(UnityEngine.Collider)
+extern void DesertControllerInteraction_OnTriggerExit_m3C3569BDE7EC9396C5FD8919DA163A873A213E62 ();
+// 0x00000006 System.Void DesertControllerInteraction::Pickup()
+extern void DesertControllerInteraction_Pickup_m54A9FBE8565FD91D66F0C0E772CF246CCFCA6100 ();
+// 0x00000007 System.Void DesertControllerInteraction::Drop()
+extern void DesertControllerInteraction_Drop_mDEC09DF1F40D713F3443CF45730D18E71EE61BB3 ();
+// 0x00000008 UnityEngine.Rigidbody DesertControllerInteraction::GetNearestRigidBody()
+extern void DesertControllerInteraction_GetNearestRigidBody_m8F0CF3D9B4087C8D48B1F42AE1006DEBD188ADA2 ();
+// 0x00000009 System.Void DesertControllerInteraction::.ctor()
+extern void DesertControllerInteraction__ctor_m4A0D7F8ABC696B24BB7E88602B97589C6FF137EE ();
+// 0x0000000A System.Void DesertFreeFlightController::Start()
+extern void DesertFreeFlightController_Start_mECFA22E8A35246C6675D57D1ED7D7C5598A3B3F5 ();
+// 0x0000000B System.Void DesertFreeFlightController::onXRChange(WebXR.WebXRState)
+extern void DesertFreeFlightController_onXRChange_m46859D0E8092C20062FDFBA0B8E469AE5A0C4DF3 ();
+// 0x0000000C System.Void DesertFreeFlightController::onXRCapabilitiesUpdate(WebXRDisplayCapabilities)
+extern void DesertFreeFlightController_onXRCapabilitiesUpdate_m6BBA5C4E63F711490B1357887FFD88058821169D ();
+// 0x0000000D System.Void DesertFreeFlightController::Update()
+extern void DesertFreeFlightController_Update_mFFE3F2A93F64FC4949B4A91F3FE7D0D433DDDE97 ();
+// 0x0000000E System.Void DesertFreeFlightController::DisableEverything()
+extern void DesertFreeFlightController_DisableEverything_m37E0E16F8551C29FE0BBD3FE768F8B264E9DA129 ();
+// 0x0000000F System.Void DesertFreeFlightController::EnableAccordingToPlatform()
+extern void DesertFreeFlightController_EnableAccordingToPlatform_m5CE4EC5C0C736E7FD1121B27E4455F18354813B5 ();
+// 0x00000010 System.Single DesertFreeFlightController::ClampAngle(System.Single,System.Single,System.Single)
+extern void DesertFreeFlightController_ClampAngle_m2171D5AA7C71F89A04E521EB217E38D9A5F6AF01 ();
+// 0x00000011 System.Void DesertFreeFlightController::.ctor()
+extern void DesertFreeFlightController__ctor_m9CB3F1D9421CEE29BE49724FA015780F90750611 ();
+static Il2CppMethodPointer s_methodPointers[17] = 
+{
+	DesertControllerInteraction_Awake_mCA7E938800DD01D086F056F146F9FC42A5C8FB85,
+	DesertControllerInteraction_Update_mB19C0D597A6804D6FCC7C34DCA2BC6706EF6F4E3,
+	DesertControllerInteraction_FixedUpdate_mB046CB6568838D4FEF9F97EC6CD2AFCE1C5C580F,
+	DesertControllerInteraction_OnTriggerEnter_m081E43E2B2ECE2A3CF11CCC0E970E3C3B3831715,
+	DesertControllerInteraction_OnTriggerExit_m3C3569BDE7EC9396C5FD8919DA163A873A213E62,
+	DesertControllerInteraction_Pickup_m54A9FBE8565FD91D66F0C0E772CF246CCFCA6100,
+	DesertControllerInteraction_Drop_mDEC09DF1F40D713F3443CF45730D18E71EE61BB3,
+	DesertControllerInteraction_GetNearestRigidBody_m8F0CF3D9B4087C8D48B1F42AE1006DEBD188ADA2,
+	DesertControllerInteraction__ctor_m4A0D7F8ABC696B24BB7E88602B97589C6FF137EE,
+	DesertFreeFlightController_Start_mECFA22E8A35246C6675D57D1ED7D7C5598A3B3F5,
+	DesertFreeFlightController_onXRChange_m46859D0E8092C20062FDFBA0B8E469AE5A0C4DF3,
+	DesertFreeFlightController_onXRCapabilitiesUpdate_m6BBA5C4E63F711490B1357887FFD88058821169D,
+	DesertFreeFlightController_Update_mFFE3F2A93F64FC4949B4A91F3FE7D0D433DDDE97,
+	DesertFreeFlightController_DisableEverything_m37E0E16F8551C29FE0BBD3FE768F8B264E9DA129,
+	DesertFreeFlightController_EnableAccordingToPlatform_m5CE4EC5C0C736E7FD1121B27E4455F18354813B5,
+	DesertFreeFlightController_ClampAngle_m2171D5AA7C71F89A04E521EB217E38D9A5F6AF01,
+	DesertFreeFlightController__ctor_m9CB3F1D9421CEE29BE49724FA015780F90750611,
+};
+static const int32_t s_InvokerIndices[17] = 
+{
+	23,
+	23,
+	23,
+	26,
+	26,
+	23,
+	23,
+	14,
+	23,
+	23,
+	32,
+	26,
+	23,
+	23,
+	23,
+	1141,
+	23,
+};
+extern const Il2CppCodeGenModule g_AssemblyU2DCSharpCodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharpCodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	17,
+	s_methodPointers,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+};
